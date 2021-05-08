ab61aadbc4abdf29e3b23b285cf8ad95097d78b9
张科家 <zhangkejia@zhangkejiadeMacBook-Pro.local>
2021-05-08 13:25:32 +0800

    feat(vite.config.js): modify vote config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
+// 使用defineConfig来获的类型提示
 export default defineConfig({
   plugins: [reactRefresh()]
 })
